1ecec7ffde510d13046b6823cc14db8a333e18fd
Sunita Ahuja <sunitaahuja@Sunitas-MacBook-Pro.local>
2025-07-08 12:34:56 +0530

    Pushing Summary Views and some changes in staging table

--- models/staging/stg_taxi_trips.sql
+++ models/staging/stg_taxi_trips.sql
@@ -19,8 +19,8 @@ unique_key
 ,ifnull(trip_miles,0.0) as trip_miles
 ,pickup_census_tract
 ,dropoff_census_tract
-,pickup_community_area
-,dropoff_community_area
+,ifnull(cast(pickup_community_area as string),"Missing Pickup Area") as pickup_community_area
+,ifnull(cast(dropoff_community_area as string),"Missing Drop Area") as dropoff_community_area
 ,ifnull(fare,0.0) as fare
 ,ifnull(tips,0.0) as tips
 ,ifnull(tolls,0.0) as tolls
@@ -35,8 +35,15 @@ unique_key
 ,dropoff_longitude
 ,dropoff_location
 ,case when coalesce(date(TIMESTAMP_ADD(trip_start_timestamp, INTERVAL (trip_seconds) SECOND)),date(trip_end_timestamp))>date(trip_start_timestamp) then 1 else 0 end as multi_day_flag
+,ST_GEOGPOINT(pickup_longitude, pickup_latitude) as pickup_geopoint
+,ST_GEOGPOINT(dropoff_longitude, dropoff_latitude) as dropoff_geopoint
+,case when date(h.date) is not null then 'Holidays' else 'Working Days' end as holiday_flag
  from 
-{{ source('taxi_trips_tdc', 'taxi_trips') }}
+{{ source('taxi_trips_tdc', 'taxi_trips') }} t
+left join {{ source('us_holiday_tdc', 'us_holiday') }} h
+on date(h.date) = date(t.trip_start_timestamp)
+and h.type like '%National holiday%'
 {% if is_incremental() %}
 WHERE date(trip_start_timestamp) > current_date - 2 -- or we can take max of trip start date -2
  {% endif %}
+ -- Taking data from Year 2020 to 2023 as 2023 is the latest Year in the dataset
---
+++ models/summary/company_payment_smry.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized='view'
+) }}
+
+Select
+extract(year from trip_start_date) as trip_start_date_year
+,FORMAT_DATE('%b', trip_start_date) as trip_start_date_month_name
+,extract(month from trip_start_date) as trip_start_date_month
+,company
+,payment_type
+,count(1) as total_trips
+,round(sum(total_trip_seconds/60),2) sum_total_trip_minutes
+,round(sum(trip_miles),2) sum_trip_miles
+,round(sum(fare),2) sum_fare
+,round(sum(tips),2) sum_tips
+,round(sum(tolls),2) sum_tolls
+,round(sum(extras),2) sum_extras
+,round(sum(trip_total),2) sum_trip_total
+,round(avg(total_trip_seconds/60),2) avg_total_trip_minutes
+,round(avg(trip_miles),2) avg_trip_miles
+,round(avg(fare),2) avg_fare
+,round(avg(tips),2) avg_tips
+,round(avg(tolls),2) avg_tolls
+,round(avg(extras),2) avg_extras
+,round(avg(trip_total),2) avg_trip_total
+,round(SAFE_DIVIDE(sum(trip_miles),sum(trip_total)),2) as fare_per_mile
+from 
+{{ ref('stg_taxi_trips') }} t
+group by 1,2,3,4,5 order by 1 desc, 3 asc,6 desc
